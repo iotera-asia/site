01f51827757444d9e06f82eac3afe881db5261a4
Maded Batara III <dev@contact.blubits.me>
2026-04-02 21:27:54 +0800

    feat: integrate Geist Variable font and update styles in Astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,21 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://iotera.asia",
+  fonts: [
+    {
+      provider: fontProviders.npm({ remote: false }),
+      name: "Geist Variable",
+      cssVariable: "--font-geist",
+      options: {
+        package: "@fontsource-variable/geist",
+      },
+      weights: ["100 900"],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
